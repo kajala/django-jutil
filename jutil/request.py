e8f61c12b083d2d5b28ff7d565abcb66555511df
Jani Kajala <kajala@gmail.com>
2018-07-07 11:28:39 +0700

    replaced freegeoip.net API with ipstack.com

--- jutil/request.py
+++ jutil/request.py
@@ -1,4 +1,5 @@
 import logging
+from django.conf import settings
 
 
 logger = logging.getLogger(__name__)
@@ -6,9 +7,13 @@ logger = logging.getLogger(__name__)
 
 def get_geo_ip(ip: str, exceptions: bool=False, timeout: int=10) -> dict:
     """
-    Returns geo IP info or empty dict if geoip query fails at http://freegeoip.net/. Example replies:
+    Returns geo IP info or empty dict if geoip query fails at http://ipstack.com.
+    requires settings.IPSTACK_TOKEN set as valid access token to the API.
+
+    Example replies:
     {'country_name': 'United States', 'country_code': 'US', 'region_code': 'TX', 'region_name': 'Texas', 'ip': '76.184.236.184', 'latitude': 33.1507, 'time_zone': 'America/Chicago', 'metro_code': 623, 'city': 'Frisco', 'longitude': -96.8236, 'zip_code': '75033'}
     {'latitude': 60.1641, 'country_name': 'Finland', 'zip_code': '02920', 'region_name': 'Uusimaa', 'city': 'Espoo', 'metro_code': 0, 'ip': '194.100.27.41', 'time_zone': 'Europe/Helsinki', 'country_code': 'FI', 'longitude': 24.7136, 'region_code': '18'}
+
     :param ip: str
     :param exceptions: if True raises Exception on failure
     :param timeout: timeout in seconds
@@ -17,10 +22,10 @@ def get_geo_ip(ip: str, exceptions: bool=False, timeout: int=10) -> dict:
     import requests
     import traceback
     try:
-        res = requests.get('http://freegeoip.net/json/{}'.format(ip), timeout=timeout)
+        res = requests.get('http://api.ipstack.com/{}?access_key={}&format=1'.format(ip, settings.IPSTACK_TOKEN), timeout=timeout)
         if res.status_code != 200:
             if exceptions:
-                raise Exception('freegeoip.net HTTP {}'.format(res.status_code))
+                raise Exception('api.ipstack.com HTTP {}'.format(res.status_code))
             return {}
         return res.json()
     except Exception as e:
