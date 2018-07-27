86704f5d6c47e8348ec2abc3dcca8fca49882fb8
Jani Kajala <kajala@gmail.com>
2018-07-27 16:39:24 -0500

    Release 1.2.10

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements('requirements.txt', session=False)
 
 setup(
     name='django-jutil',
-    version='1.2.9',
+    version='1.2.10',
     author=u'Jani Kajala',
     author_email='kajala@gmail.com',
     packages=find_packages(exclude=['project', 'venv']),
