9fe26e65f6392e97cabb3f498159fe7305716b2c
Jani Kajala <kajala@gmail.com>
2018-10-01 12:57:48 -0500

    selective cached fields update

--- jutil/cache.py
+++ jutil/cache.py
@@ -15,34 +15,33 @@ class CachedFieldsMixin(object):
     """
     cached_fields = []
 
-    def is_cache_update(self, update_fields: list) -> bool:
-        """
-        Returns True if specified field update list matches exactly cached fields update
-        :param update_fields: List of fields saved in this update
-        :return: bool
-        """
-        return update_fields and sorted(list(update_fields)) == sorted(list(self.cached_fields))
-
-    def update_cached_fields(self, commit: bool=True, exceptions: bool=True):
+    def update_cached_fields(self, commit: bool=True, exceptions: bool=True, updated_fields: list=None):
         try:
             """
-            :param updated_fields: List of fields to update. Pass None for all.
+            Updates cached fields using get_xxx calls for each cached field (in cached_fields list).
             :param commit: Save update fields to DB
+            :param exceptions: Raise exceptions or not
+            :param updated_fields: List of cached fields to update. Pass None for all cached fields.
             """
-            for k in self.cached_fields:
+            fields = updated_fields or self.cached_fields
+            for k in fields:
                 f = 'get_' + k
                 if not hasattr(self, f):
                     raise Exception('Field {} marked as cached in {} but function get_{}() does not exist'.format(k, self, k))
                 v = self.__getattribute__(f)()
                 setattr(self, k, v)
             if commit:
-                self.save(update_fields=self.cached_fields)
+                self.save(update_fields=fields)
         except Exception as e:
             logger.error('{}.update_cached_fields: {}'.format(self.__class__, e))
             if exceptions:
                 raise e
 
     def update_cached_fields_pre_save(self, update_fields: list):
+        """
+        Call on pre_save signal for objects (to automatically refresh on save).
+        :param update_fields: list of fields to update
+        """
         if self.id and update_fields is None:
             self.update_cached_fields(commit=False, exceptions=False)
 
