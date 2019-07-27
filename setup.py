ecd5ac0805f9de48ec9227f7821b77787aff2469
Jani Kajala <kajala@gmail.com>
2019-07-27 13:44:22 -0500

    Release 2.2.18

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements('requirements.txt', session=False)
 
 setup(
     name='django-jutil',
-    version='2.2.17',
+    version='2.2.18',
     author=u'Jani Kajala',
     author_email='kajala@gmail.com',
     packages=find_packages(exclude=['project', 'venv']),
