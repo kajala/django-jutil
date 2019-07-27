fc5f184884e0ee4424b18a4cca8671b210f73117
Jani Kajala <kajala@gmail.com>
2019-07-27 13:42:08 -0500

    added format_xml command

---
+++ jutil/management/commands/format_xml.py
@@ -0,0 +1,15 @@
+from django.core.management.base import BaseCommand, CommandParser
+from jutil.format import format_xml_file
+
+
+class Command(BaseCommand):
+    help = 'Formats XML file'
+
+    def add_arguments(self, parser: CommandParser):
+        parser.add_argument('filename', type=str)
+        parser.add_argument('--encoding', type=str, default='UTF-8')
+
+    def handle(self, **options):
+        filename = options['filename']
+        encoding = options['encoding']
+        print(format_xml_file(filename, encoding=encoding).decode())
