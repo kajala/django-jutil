00d631451a171bd68182b025750787038ac59651
Jani Kajala <kajala@gmail.com>
2019-10-16 11:46:32 -0500

    Release 2.2.31

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements('requirements.txt', session=False)
 
 setup(
     name='django-jutil',
-    version='2.2.30',
+    version='2.2.31',
     author=u'Jani Kajala',
     author_email='kajala@gmail.com',
     packages=find_packages(exclude=['project', 'venv']),
