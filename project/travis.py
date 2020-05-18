305968698002aa3656511996883ce8bd66cb7bf1
Jani Kajala <kajala@gmail.com>
2020-05-18 16:51:16 -0500

    travis log changes

--- project/travis.py
+++ project/travis.py
@@ -25,3 +25,39 @@ DATABASES = {
 
 # IP stack (IP country code)
 IPSTACK_TOKEN = ''
+
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'ndebug': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+    },
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%Y-%m-%d %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout,
+        }
+    },
+    'loggers': {
+        'jutil': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'WARNING',
+        },
+    }
+}
