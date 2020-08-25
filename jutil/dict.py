13b9b32bccc2362072cd65277125e5f5a35ea808
Jani Kajala <kajala@gmail.com>
2020-08-24 19:47:06 -0500

    deleted deprecated dict.choices_label and dict.dict_to_html, use format.choices_label and format.format_dict_as_html

--- jutil/dict.py
+++ jutil/dict.py
@@ -1,6 +1,6 @@
 import logging
 from collections import OrderedDict
-from typing import Sequence, Tuple, Dict, TypeVar, Any
+from typing import Dict, TypeVar
 
 R = TypeVar('R')
 S = TypeVar('S')
@@ -15,15 +15,3 @@ def sorted_dict(d: Dict[S, R]) -> Dict[S, R]:
     :return: OrderedDict
     """
     return OrderedDict(sorted(d.items()))
-
-
-def choices_label(choices: Sequence[Tuple[S, str]], value: S) -> str:
-    logger.warning('jutil.dict.choices_label is deprecated, use jutil.format.choices_label')
-    from jutil.format import choices_label as format_choices_label  # noqa  # type: ignore
-    return format_choices_label(choices, value)
-
-
-def dict_to_html(data: Dict[str, Any], format_keys: bool = True) -> str:
-    logger.warning('jutil.dict.dict_to_html is deprecated, use jutil.format.format_dict_as_html')
-    from jutil.format import format_dict_as_html  # noqa  # type: ignore
-    return format_dict_as_html(data, format_keys)
