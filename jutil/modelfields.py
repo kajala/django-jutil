96932bb9fc28a817cd8426a9383938d7cdfb8074
Jani Kajala <kajala@gmail.com>
2021-07-04 09:10:32 -0500

    mypy_django_plugin tweaks

--- jutil/modelfields.py
+++ jutil/modelfields.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.db import models
 from django.utils.html import strip_tags
 
@@ -7,6 +8,10 @@ class SafeCharField(models.CharField):
     CharField which strips HTML tags from form data on save.
     """
 
+    _pyi_private_set_type: str  # for mypy_django_plugin
+    _pyi_private_get_type: str  # for mypy_django_plugin
+    _pyi_lookup_exact_type: Any  # for mypy_django_plugin
+
     def save_form_data(self, instance, data):
         setattr(instance, self.name, strip_tags(str(data)) if data else data)
 
@@ -15,6 +20,10 @@ class SafeTextField(models.TextField):
     """
     TextField which strips HTML tags from form data on save.
     """
+
+    _pyi_private_set_type: str  # for mypy_django_plugin
+    _pyi_private_get_type: str  # for mypy_django_plugin
+    _pyi_lookup_exact_type: Any  # for mypy_django_plugin
 
     def save_form_data(self, instance, data):
         setattr(instance, self.name, strip_tags(str(data)) if data else data)
