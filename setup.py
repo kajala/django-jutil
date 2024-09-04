acc0037132509183f8294b2481a583e4828b5e63
Jani Kajala <kajala@gmail.com>
2024-09-04 14:29:24 -0500

    Release 4.1.12

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ install_requires = parse_requirements("requirements.txt", session=False)
 
 setup(
     name="django-jutil",
-    version="4.1.11",
+    version="4.1.12",
     author="Jani Kajala",
     author_email="kajala@gmail.com",
     packages=find_packages(exclude=["project", "venv"]),
